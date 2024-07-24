f1e8e9467333d2777b5b3f5c4c1ce439aa9703a0
Alexey Popkov <alexey.popkov@pelc-consult.com>
2024-07-24 10:45:43 +0300

    Initial Commit

---
+++ router_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  router_example
+//
+//  Created by Алексей Попков on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
